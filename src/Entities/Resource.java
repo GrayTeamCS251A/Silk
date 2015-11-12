93a273aa19ffa82de269cc02d9187498a43ecd1b
jayashree238 <jayashreeprabunathan2308@gmail.com>
2015-11-11 17:17:49 -0800

    Updated Resource.class
    Defined methods

--- src/Entities/Resource.java
+++ src/Entities/Resource.java
@@ -2,49 +2,72 @@ package Entities;
 
 import java.util.*;
 
-/**
- * 
- */
+
 public class Resource extends Observable {
-
-    /**
-     * 
-     */
-    public Resource() {
-    }
-
-    /**
-     * 
-     */
+	
     private String name;
-
-    /**
-     * 
-     */
     private double dailyCost;
-
-    /**
-     * 
-     */
     private int resourceID;
-
-    /**
-     * 
-     */
     private ResourceType type;
 
-    /**
-     * 
-     */
-    public void getResourceInfo() {
-        // TODO implement here
+    public Resource(int resourceID, String name, double dailyCost, ResourceType type){
+        this.resourceID= resourceID;
+        this.name = name;
+        this.dailyCost = dailyCost;
+        this.type = type;
+    }
+    
+    public Resource() {
+           }
+
+    public int getResourceID(){
+         return resourceID;
+     }
+     
+     public String getname(){
+         return name;
+     }
+     public double getDailyCost(){
+         return dailyCost;
+     }
+     
+     public ResourceType getResourceType(){
+         return type;
+     }
+     
+     public void getResourceInfo() {
+        getResourceID();
+        getname();
+        getDailyCost();
+        getResourceType();
     }
 
-    /**
-     * 
-     */
-    public void updateResourceInfo() {
-        // TODO implement here
+     public void setResourceID(int resourceID){
+        this.resourceID = resourceID;
+    }
+    
+     public void setname(String name){
+        this.name = name;
+    }
+    
+     public void setDailyCost(double dailyCost){
+        this.dailyCost = dailyCost;
+    }
+    
+     public void setResourceType(ResourceType type){
+        this.type = type;
+    }
+    
+     public void updateResourceInfo(resourceID,name,DailyCost,resourceType){
+        if (resourceID !=null)
+        	setResourceID(resourceID);
+        if (name != null)
+        	setname
+        	(name);
+        if (DailyCost != null)
+        	setDailyCost(dailyCost);
+        if (resourceType != null)
+        	setResourceType(type);
     }
 
 }
