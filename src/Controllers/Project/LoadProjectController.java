71b88cc8fe4ce3f2a6bbaae635241aa82b94cd38
Peter Chou <peterchou@peters-mbp.attlocal.net>
2015-11-14 15:18:29 -0800

    added the save to xml and load from xml functionality

--- src/Controllers/Project/LoadProjectController.java
+++ src/Controllers/Project/LoadProjectController.java
@@ -1,10 +1,24 @@
 package Controllers.Project;
 
 import java.io.File;
+import java.io.IOException;
+import java.sql.Time;
 import java.util.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import AnalysisModel.Boundaries.Panels.Oracle;
 import Controllers.Controller;
+import Entities.Deliverable;
+import Entities.ResourceType;
+import Entities.Task;
 
 /**
  * 
@@ -20,12 +34,285 @@ public class LoadProjectController implements Controller {
     /**
      * @param file
      */
-    private void parse(File file) {
+    private void parse(File projectFile) {
 		//XML grab the project info
 		//project.updateInfo(projectName, startTime, projectAuthor);
 		
 		//XML grab the project other stuff
 		//project.updateProject(resources, tasks, schedule);
+    	
+    	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+		try {
+
+			//Using factory get an instance of document builder
+			DocumentBuilder db = dbf.newDocumentBuilder();
+
+			//parse using builder to get DOM representation of the XML file
+			Document projectDocument = db.parse(projectFile);
+
+			Element projectRootElement = projectDocument.getDocumentElement();
+			
+			//*************************
+			//*   Read Project Info   *
+			//*************************
+			NodeList projectInfoNodeList = projectRootElement.getElementsByTagName("ProjectInfo");
+			String projectName = "";
+			String projectAuthor = "";
+			String projectStartTime = "";
+			String projectEndTime = "";
+			if(projectInfoNodeList != null) {
+				//Get Project Info Element
+				Element projectInfo = (Element) projectInfoNodeList.item(0);
+				
+				NodeList projectNameInfoList = projectInfo.getElementsByTagName("ProjectName");
+				if(projectNameInfoList != null && projectNameInfoList.getLength() > 0) {
+					Element el = (Element)projectNameInfoList.item(0);
+					projectName = el.getFirstChild().getNodeValue();
+				}
+				
+				NodeList projectAuthorInfoList = projectInfo.getElementsByTagName("ProjectAuthor");
+				if(projectAuthorInfoList != null && projectAuthorInfoList.getLength() > 0) {
+					Element el = (Element)projectAuthorInfoList.item(0);
+					projectAuthor = el.getFirstChild().getNodeValue();
+				}
+				
+				NodeList projectStartTimeInfoList = projectInfo.getElementsByTagName("ProjectStartTime");
+				if(projectAuthorInfoList != null && projectStartTimeInfoList.getLength() > 0) {
+					Element el = (Element)projectStartTimeInfoList.item(0);
+					projectStartTime = el.getFirstChild().getNodeValue();
+				}
+				
+				NodeList projectEndTimeInfoList = projectInfo.getElementsByTagName("ProjectEndTime");
+				if(projectAuthorInfoList != null && projectEndTimeInfoList.getLength() > 0) {
+					Element el = (Element)projectEndTimeInfoList.item(0);
+					projectEndTime = el.getFirstChild().getNodeValue();
+				}
+				
+				project.updateInfo(projectName, projectStartTime, projectAuthor);
+				project.setEndTime(new Time(Integer.parseInt(projectEndTime)));
+			}
+			
+			//**************************
+			//*   Read Resource Info   *
+			//**************************
+			NodeList projectResourcesNodeList = projectRootElement.getElementsByTagName("ProjectResources");
+						
+			if(projectResourcesNodeList != null && projectResourcesNodeList.getLength() > 0) {
+					//get the Project Resource root element
+				Element resourceInfoList = (Element) projectResourcesNodeList.item(0);
+				
+				NodeList projectResourceNodeList = resourceInfoList.getElementsByTagName("Resource");
+				for(int i = 0 ; i < projectResourceNodeList.getLength(); i++) {
+					String resourceName = "";
+					Integer resourceID = 0;
+					double dailyCost = 0.0;
+					ResourceType r = null; 
+					
+					Element resourceInfo = (Element) projectResourceNodeList.item(i);
+					
+					NodeList resourceNameInfoList = resourceInfo.getElementsByTagName("ResourceName");
+					if(resourceNameInfoList != null && resourceNameInfoList.getLength() > 0) {
+						Element el = (Element)resourceNameInfoList.item(0);
+						resourceName = el.getFirstChild().getNodeValue();
+					}
+					
+					resourceID = Integer.parseInt(resourceInfo.getAttribute("id"));
+					
+					NodeList resourceDailyCostInfoList = resourceInfo.getElementsByTagName("ResourceDailyCost");
+					if(resourceDailyCostInfoList != null && resourceDailyCostInfoList.getLength() > 0) {
+						Element el = (Element)resourceDailyCostInfoList.item(0);
+						dailyCost = Double.parseDouble(el.getFirstChild().getNodeValue());
+					}
+					
+					NodeList resourceTypeInfoList = resourceInfo.getElementsByTagName("ResourceType");
+					if(resourceTypeInfoList != null && resourceTypeInfoList.getLength() > 0) {
+						Element el = (Element)resourceTypeInfoList.item(0);
+						String type = el.getFirstChild().getNodeValue();
+						
+						switch(type) {
+							case "ResourceType.labor": r = ResourceType.labor;
+								break;
+							case "ResourceType.equipment": r = ResourceType.equipment;
+								break;
+							case "ResourceType.material": r = ResourceType.material;
+								break;
+						}
+					}
+					
+					project.createResource(resourceName, resourceID, dailyCost, r);
+				}
+			}
+			
+			//**********************
+			//*   Read Task Info   *
+			//**********************
+			NodeList projectTasksNodeList = projectRootElement.getElementsByTagName("ProjectTasks");
+			
+			if(projectTasksNodeList != null && projectTasksNodeList.getLength() > 0) {
+				//get the Project Task element
+				Element taskInfoList = (Element)projectTasksNodeList.item(0);
+					
+				NodeList projectTaskNodeList = taskInfoList.getElementsByTagName("Task");
+				
+				for(int i = 0 ; i < projectTaskNodeList.getLength(); i++) {
+					String taskName = "";
+					Integer taskID = 0;
+					String taskDescription = "";
+					Integer taskDuration = 0;
+					double percentCompleted = 0.0;
+					String taskStartTime = "";
+					String taskEndTime = "";
+					String parentTaskName = "";
+					List<String> listOfTaskPredecessorNames = new LinkedList<String>();
+					List<Deliverable> listOfTaskDeliverables = new LinkedList<Deliverable>();
+					List<String> listOfTaskResourceNames = new LinkedList<String>();
+					
+					Element taskInfo = (Element) projectTaskNodeList.item(i);
+					
+					NodeList taskNameInfoList = taskInfo.getElementsByTagName("TaskName");
+					if(taskNameInfoList != null && taskNameInfoList.getLength() > 0) {
+						Element el = (Element)taskNameInfoList.item(0);
+						taskName = el.getFirstChild().getNodeValue();
+					}
+					
+					taskID = Integer.parseInt(taskInfo.getAttribute("id"));
+					
+					NodeList taskDescriptionInfoList = taskInfo.getElementsByTagName("TaskDescription");
+					if(taskDescriptionInfoList != null && taskDescriptionInfoList.getLength() > 0) {
+						Element el = (Element)taskDescriptionInfoList.item(0);
+						taskDescription = el.getFirstChild().getNodeValue();
+					}
+					
+					NodeList taskDurationInfoList = taskInfo.getElementsByTagName("TaskDuration");
+					if(taskDurationInfoList != null && taskDurationInfoList.getLength() > 0) {
+						Element el = (Element)taskDurationInfoList.item(0);
+						taskDuration = Integer.parseInt(el.getFirstChild().getNodeValue());
+					}
+					
+					NodeList taskPercentCompletedInfoList = taskInfo.getElementsByTagName("TaskPercentCompleted");
+					if(taskPercentCompletedInfoList != null && taskPercentCompletedInfoList.getLength() > 0) {
+						Element el = (Element)taskPercentCompletedInfoList.item(0);
+						percentCompleted = Double.parseDouble(el.getFirstChild().getNodeValue());
+					}
+					
+					NodeList taskStarTimeInfoList = taskInfo.getElementsByTagName("TaskStartTime");
+					if(taskStarTimeInfoList != null && taskStarTimeInfoList.getLength() > 0) {
+						Element el = (Element)taskStarTimeInfoList.item(0);
+						taskStartTime = el.getFirstChild().getNodeValue();
+					}
+					
+					NodeList taskEndTimeInfoList = taskInfo.getElementsByTagName("TaskEndTime");
+					if(taskEndTimeInfoList != null && taskEndTimeInfoList.getLength() > 0) {
+						Element el = (Element)taskEndTimeInfoList.item(0);
+						taskEndTime = el.getFirstChild().getNodeValue();
+					}
+					
+					NodeList taskParentTaskInfoList = taskInfo.getElementsByTagName("TaskParentTask");
+					if(taskParentTaskInfoList != null && taskParentTaskInfoList.getLength() > 0) {
+						Element el = (Element)taskParentTaskInfoList.item(0);
+						parentTaskName = el.getFirstChild().getNodeValue();
+					}
+					
+					NodeList taskPredecessorsInfoList = taskInfo.getElementsByTagName("TaskPredecessors");
+					if(taskPredecessorsInfoList != null && taskPredecessorsInfoList.getLength() > 0) {
+						Element taskPredInfo = (Element)taskPredecessorsInfoList.item(0);
+						
+						NodeList taskPredInfoList = taskPredInfo.getElementsByTagName("TaskPredecessor");
+						for (int y = 0; y < taskPredInfoList.getLength(); y++)
+						{
+							String predTaskName = "";
+							Element el = (Element)taskPredInfoList.item(0);
+							predTaskName = el.getFirstChild().getNodeValue();
+							
+							listOfTaskPredecessorNames.add(predTaskName);
+						}
+					}
+					
+					NodeList taskDeliverablesInfoList = taskInfo.getElementsByTagName("TaskDeliverables");
+					if(taskDeliverablesInfoList != null && taskDeliverablesInfoList.getLength() > 0) {
+						Element taskDeliverableInfo = (Element)taskDeliverablesInfoList.item(0);
+						
+						NodeList taskDelInfoList = taskDeliverableInfo.getElementsByTagName("TaskDeliverable");
+						for (int y = 0; y < taskDelInfoList.getLength(); y++)
+						{
+							Element delElement = (Element) taskDelInfoList.item(y);
+							
+							String deliverableName = "";
+							String deliverableType = "";
+							
+							NodeList taskDeliverableNameInfo = delElement.getElementsByTagName("TaskDeliverableName");
+							if(taskParentTaskInfoList != null && taskDeliverableNameInfo.getLength() > 0) {
+								Element el = (Element)taskDeliverableNameInfo.item(0);
+								deliverableName = el.getFirstChild().getNodeValue();
+							}
+							
+							NodeList taskDeliverableTypeInfo = delElement.getElementsByTagName("TaskDeliverableType");
+							if(taskParentTaskInfoList != null && taskDeliverableTypeInfo.getLength() > 0) {
+								Element el = (Element)taskDeliverableTypeInfo.item(0);
+								deliverableType = el.getFirstChild().getNodeValue();
+							}
+							
+							listOfTaskDeliverables.add(project.createDeliverable(deliverableName, deliverableType));	
+						}
+					}
+					
+					NodeList taskResourcesInfoList = taskInfo.getElementsByTagName("TaskResources");
+					if(taskResourcesInfoList != null && taskResourcesInfoList.getLength() > 0) {
+						Element taskResourceInfo = (Element)taskResourcesInfoList.item(0);
+						
+						NodeList taskResInfoList = taskResourceInfo.getElementsByTagName("TaskDeliverable");
+						for (int y = 0; y < taskResInfoList.getLength(); y++)
+						{
+							Element resElement = (Element) taskResInfoList.item(y);
+							
+							String taskResName = "";
+							NodeList taskResNameInfo = resElement.getElementsByTagName("TaskResource");
+							if(taskParentTaskInfoList != null && taskResNameInfo.getLength() > 0) {
+								Element el = (Element)taskResNameInfo.item(0);
+								taskResName = el.getFirstChild().getNodeValue();
+							}
+							
+							listOfTaskResourceNames.add(taskResName);	
+						}
+					}
+					
+					
+					project.createTaskfromLoadProject(taskName, taskID, taskDuration);
+					project.getTask(taskName).setParentTask(project.getTask(parentTaskName));
+					project.getTask(taskName).setTaskDescription(taskDescription);
+					project.getTask(taskName).setPercentCompleted(percentCompleted);
+					project.getTask(taskName).setStartTime(taskStartTime);
+					project.getTask(taskName).setEndTime(taskEndTime);
+					
+					//Set the list of predecessor tasks
+					for (int l = 0; l < listOfTaskPredecessorNames.size(); l++)
+					{
+						project.getTask(taskName).addPredecessor(project.getTask(listOfTaskPredecessorNames.get(l)));
+					}
+					
+					//Set the list of deliverables for the task
+					for (int d = 0; d < listOfTaskDeliverables.size(); d++)
+					{
+						project.getTask(taskName).addDeliverable(listOfTaskDeliverables.get(d));
+					}
+					
+					//Set the list of resources for the task
+					for (int r = 0; r < listOfTaskResourceNames.size(); r++)
+					{
+						project.getTask(taskName).addResource(project.getResource(listOfTaskResourceNames.get(r)));
+					}
+				}
+			}
+			
+
+		}catch(ParserConfigurationException pce) {
+			pce.printStackTrace();
+		}catch(SAXException se) {
+			se.printStackTrace();
+		}catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
     }
 
 	@Override
--- src/Controllers/Project/SaveProjectController.java
+++ src/Controllers/Project/SaveProjectController.java
@@ -5,7 +5,26 @@ import java.util.*;
 
 import AnalysisModel.Boundaries.Panels.Oracle;
 import Controllers.*;
+import Entities.Deliverable;
 import Entities.Project;
+import Entities.Resource;
+import Entities.Task;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * 
@@ -22,8 +41,185 @@ public class SaveProjectController implements Controller {
      * @param project 
      * @return
      */
-    private File convert(Project project, String fileName) {
-        // Convert to XML format to the specified fileName
+    private static File convert(Project project, String fileName) {
+        // Convert Project to XML format to the specified fileName
+    	try {
+
+    		DocumentBuilderFactory xmlDocument = DocumentBuilderFactory.newInstance();
+    		DocumentBuilder xmlBuilder = xmlDocument.newDocumentBuilder();
+    		Document doc = xmlBuilder.newDocument();
+    		
+    		Element mainRootProject = doc.createElement("Silk");
+
+    		//***********************************
+    		//*		Project Info XML Elements   *
+    		//***********************************
+    		Element projectInfoElementRoot = doc.createElement("ProjectInfo");
+    		doc.appendChild(projectInfoElementRoot);
+
+    		Element projectName = doc.createElement("ProjectName");
+    		projectName.appendChild(doc.createTextNode(project.getProjectName()));
+    		
+    		Element projectAuthor = doc.createElement("ProjectAuthor");
+    		projectAuthor.appendChild(doc.createTextNode(project.getProjectAuthor()));
+    		
+    		Element projectStartTime = doc.createElement("ProjectStartTime");
+    		projectStartTime.appendChild(doc.createTextNode(project.getStartTime().toString()));
+    		
+    		Element projectEndTime = doc.createElement("ProjectEndTime");
+    		projectEndTime.appendChild(doc.createTextNode(project.getEndTime().toString()));
+    		
+    		projectInfoElementRoot.appendChild(projectName);
+    		projectInfoElementRoot.appendChild(projectAuthor);
+    		projectInfoElementRoot.appendChild(projectStartTime);
+    		projectInfoElementRoot.appendChild(projectEndTime);
+    		
+    		//***************************************
+    		//*		Project Resource XML Elements   *
+    		//***************************************
+    		Set<Resource> projectResources = project.getResources();
+    		
+    		Element projectResourceElementsRoot = doc.createElement("ProjectResources");
+    		
+    		for (Resource r: projectResources) {
+    			Element prResRoot = doc.createElement("Resource");
+    			prResRoot.setAttribute("id", String.valueOf(r.getResourceID()));
+    			
+    			Element prResName = doc.createElement("ResourceName");
+    			prResName.appendChild(doc.createTextNode(r.getname()));
+    			
+    			Element prResDailyCost = doc.createElement("ResourceDailyCost");
+    			prResDailyCost.appendChild(doc.createTextNode(String.valueOf(r.getDailyCost())));
+    			
+    			Element prResType = doc.createElement("ResourceType");
+    			prResType.appendChild(doc.createTextNode(String.valueOf(r.getResourceType())));
+    			
+    			prResRoot.appendChild(prResName);
+    			prResRoot.appendChild(prResDailyCost);
+    			prResRoot.appendChild(prResType);
+    			
+    			projectResourceElementsRoot.appendChild(prResRoot);
+    		}
+    		
+    		//***********************************
+    		//*		Project Task XML Elements   *
+    		//***********************************
+    		Set<Task> projectTasks = project.getTasks();
+    		
+    		Element projectTaskElementRoot = doc.createElement("ProjectTasks");
+    		
+    		for (Task t: projectTasks) {
+    			Element prTaskRoot = doc.createElement("Task");
+    			prTaskRoot.setAttribute("id", String.valueOf(t.getTaskID()));
+    			
+    			Element prTaskName = doc.createElement("TaskName");
+    			prTaskName.appendChild(doc.createTextNode(t.getTaskName()));
+    			
+    			Element prTaskDescription = doc.createElement("TaskDescription");
+    			prTaskDescription.appendChild(doc.createTextNode(t.getTaskDescription()));
+    			
+    			Element prTaskDuration = doc.createElement("TaskDuration");
+    			prTaskDuration.appendChild(doc.createTextNode(String.valueOf(t.getTaskDuration())));
+    			
+    			Element prStartTime = doc.createElement("TaskStartTime");
+    			prStartTime.appendChild(doc.createTextNode(String.valueOf(t.getStartTime())));
+    			
+    			Element prEndTime = doc.createElement("TaskEndTime");
+    			prEndTime.appendChild(doc.createTextNode(String.valueOf(t.getEndTime())));
+    			
+    			Element prPercentCompleted = doc.createElement("TaskPercentCompleted");
+    			prPercentCompleted.appendChild(doc.createTextNode(String.valueOf(t.getPercentCompleted())));
+    			
+    			Element prParentTask = doc.createElement("TaskParentTask");
+    			prParentTask.appendChild(doc.createTextNode(t.getParentTask().getTaskName()));
+    			
+    			Element prTaskPredRoot = doc.createElement("TaskPredecessors");
+    			int index = 1;
+    			//Create an Element for each Predecessor
+    			for (Task pred: t.getTaskPredecessors())
+    			{
+    				Element prTaskPred = doc.createElement("TaskPredecessor");
+    				prTaskPred.setAttribute("id", String.valueOf(index));
+    				prTaskPred.appendChild(doc.createTextNode(pred.getTaskName()));
+    				
+    				prTaskPredRoot.appendChild(prTaskPred);
+    				index++;
+    			}
+    			
+    			Element prTaskDeliverableRoot = doc.createElement("TaskDeliverables");
+    			index = 1;
+    			for (Deliverable d: t.getDeliverables())
+    			{
+    				Element prTaskDeliverable = doc.createElement("TaskDeliverable");
+    				prTaskDeliverable.setAttribute("id", String.valueOf(index));
+    				
+    				Element prTaskDeliverableName = doc.createElement("TaskDeliverableName");
+    				prTaskDeliverableName.appendChild(doc.createTextNode(d.getDeliverableName()));
+    				
+    				Element prTaskDeliverableType = doc.createElement("TaskDeliverableType");
+    				prTaskDeliverable.appendChild(doc.createTextNode(String.valueOf(d.getDeliverableType())));
+    				
+    				
+    				prTaskDeliverable.appendChild(prTaskDeliverableName);
+    				prTaskDeliverable.appendChild(prTaskDeliverableType);
+    				
+    				prTaskDeliverableRoot.appendChild(prTaskDeliverable);
+    				index++;
+    			}
+    			
+    			Element prTaskResourcesRoot = doc.createElement("TaskResources");
+    			index = 1;
+    			for (Resource r: t.getTaskResources())
+    			{
+    				Element prTaskResource = doc.createElement("TaskResource");
+    				prTaskResource.setAttribute("id", String.valueOf(index));
+    				prTaskResource.appendChild(doc.createTextNode(r.getname()));
+    				
+    				prTaskResourcesRoot.appendChild(prTaskResource);
+    				index++;
+    			}
+    			
+    			prTaskRoot.appendChild(prTaskName);
+    			prTaskRoot.appendChild(prTaskDescription);
+    			prTaskRoot.appendChild(prTaskDuration);
+    			prTaskRoot.appendChild(prStartTime);
+    			prTaskRoot.appendChild(prEndTime);
+    			prTaskRoot.appendChild(prPercentCompleted);
+    			prTaskRoot.appendChild(prParentTask);
+    			prTaskRoot.appendChild(prTaskPredRoot);
+    			prTaskRoot.appendChild(prTaskDeliverableRoot);
+    			prTaskRoot.appendChild(prTaskResourcesRoot);
+    			
+    			projectTaskElementRoot.appendChild(prTaskRoot);
+    		}
+
+    		
+    		
+    		
+    		mainRootProject.appendChild(projectInfoElementRoot);
+    		mainRootProject.appendChild(projectResourceElementsRoot);
+    		mainRootProject.appendChild(projectTaskElementRoot);
+    		
+    		// write the content into xml file
+    		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    		Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes"); 
+    		DOMSource source = new DOMSource(doc);
+    		StreamResult result = new StreamResult(new File(fileName + ".xml"));
+    		//System.out.println("File saved!");
+
+    		
+    		// Output to console for testing
+    		//StreamResult result = new StreamResult(System.out);
+    		transformer.transform(source, result);
+
+
+    	  } catch (ParserConfigurationException pce) {
+    		pce.printStackTrace();
+    	  } catch (TransformerException tfe) {
+    		tfe.printStackTrace();
+    	  }
+    	
         return null;
     }
 
@@ -36,5 +232,10 @@ public class SaveProjectController implements Controller {
 	public void executeSaveProject(String fileName) {
 		convert(project, fileName);
 	}
-
+/*
+	public static final void main(String argv[])
+	{
+		convert(project, "testing");
+	}
+*/
 }
