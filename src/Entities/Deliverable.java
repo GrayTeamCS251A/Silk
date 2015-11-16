e712131b2c30fd22127be3d1b3a54ac5cd52fcc7
jayashree238 <jayashreeprabunathan2308@gmail.com>
2015-11-16 12:36:17 -0800

    combined DeliverableType class
    And added getters and setters.
    Deleted DeliverableType class as I included the enum here.

--- src/Entities/Deliverable.java
+++ src/Entities/Deliverable.java
@@ -7,20 +7,30 @@ import java.util.*;
  */
 public class Deliverable {
 
-    /**
-     * 
-     */
-    public Deliverable() {
-    }
-
-    /**
-     * 
-     */
-    private String name;
-
-    /**
-     * 
-     */
+	public enum DeliverableType {
+	    file,
+	    presentation
+	}
+	private String name;
     private DeliverableType type;
 
+    public Deliverable(String name, DeliverableType type) {
+    	this.name = name;
+    	this.type = type;
+    }
+    public String getName(){
+    	return this.name;
+    }
+    public DeliverableType getType(){
+    	return this.type;
+    }
+    
+    public void setName(String name){
+    	this.name = name;
+    	
+    }
+    public void setType(DeliverableType type){
+    	this.type = type;
+    }
+    
 }
