e8441510b7fb45720479cfde765af51ff4c877a7
jayashree238 <jayashreeprabunathan2308@gmail.com>
2015-11-16 13:15:19 -0800

    Update Task.java

--- src/Entities/Task.java
+++ src/Entities/Task.java
@@ -14,7 +14,7 @@ public class Task extends Observable {
 	    private double endTime;
 	    private double percentCompleted;
 	    private int taskID;
-	    private Set<Deiverable> deliverables;
+	    private Set<Deliverable> deliverables;
 	    private Set<Resource> requiredResources;
 	    private Set<Task> predecessors;
 
@@ -34,7 +34,7 @@ public class Task extends Observable {
    public int getTaskID(){
 	   return taskID;
    }
-   public String getTaskname(){
+   public String getTaskName(){
 	   return name;
    }
    public String getDescription(){
@@ -53,8 +53,8 @@ public class Task extends Observable {
    public double getPercentCompleted(){
 	   return percentCompleted;
    }
-   public Set<Deiverable> getDeliverables(){
-	   return deliverables;
+   public Set<Deliverable> getDeliverables(){
+	  return deliverables;
    }
    public Set<Resource> getRequiredResources(){
 	   return requiredResources;
@@ -67,7 +67,7 @@ public class Task extends Observable {
    }
    public void getTaskInfo(){
 	   getTaskID();
-	   getTaskname();
+	   getTaskName();
 	   getDescription();
 	   getDuration();
 	   getStartTime();
@@ -82,13 +82,13 @@ public class Task extends Observable {
    public void setTaskID(int taskID){
 	   this.taskID = taskID;
    }
-   public void setTaskname(String name){
+   public void setName(String name){
 	   this.name= name;
    }
    public void setDescription(String description){
 	   this.description = description;
    }
-   public void setDuration(int duration){
+   public void setTaskDuration(int duration){
 	   this.duration= duration;
    }
    public void setStartTime(double startTime){
@@ -101,34 +101,34 @@ public class Task extends Observable {
    public void setPercentCompleted(double percentCompleted){
 	   this.percentCompleted = percentCompleted;
    }
-   public void setDeliverables(Set<Deiverable> deliverables){
-	   this.deliverables = deliverables;
+   public void setDeliverables(Set<Deliverable> deliverables){
+	  this.deliverables = deliverables;
    }
    public void setRequiredResources(Set<Resource> requiredResources){
 	   this.requiredResources = requiredResources;
    }
-   public void setPredecessors(Set<Task> predecessors){
+   public void setPredecessorTask(Set<Task> predecessors){
 	   this.predecessors = predecessors;
    }
-   public void setParent(Task parent){
+   public void setTaskParent(Task parent){
 	   this.parent = parent;
    }
     /**
      * @param info
      */
-    public void updateTask(int taskID, String name, String description, int duration, double startTime, double endTime, double percentCompleted, Set<Deiverable> deliverables, Set<Resource> requiredResources, Set<Task> predecessors, Task parent) {
+    public void updateTask(int taskID, String name, String description, int duration, double startTime, double endTime, double percentCompleted, Set<Resource> requiredResources, Set<Task> predecessors, Task parent) {
         // TODO implement here
     	setTaskID(taskID);
-    	setTaskname(name);
+    	setTaskName(name);
     	setDescription(description);
-    	setDuration(duration);
+    	setTaskDuration(duration);
     	setStartTime(startTime);
     	setEndTime(endTime);
     	setPercentCompleted(percentCompleted);
-    	setDeliverables(deliverables);
+    	//setDeliverables(deliverables);
     	setRequiredResources(requiredResources);
-    	setPredecessors(predecessors);
-    	setParent(parent);
+    	setPredecessorTask(predecessors);
+    	setTaskParent(parent);
     		
     }
 
