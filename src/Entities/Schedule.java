ffa14b85ab401df0a22765f9af1b087f1264b330
twostripe <macarpenter@gmail.com>
2015-11-17 13:17:56 -0800

    Added Resources & Updated Schedule

--- src/Entities/Schedule.java
+++ src/Entities/Schedule.java
@@ -1,6 +1,8 @@
 package Entities;
 
 import java.util.*;
+import jxl.*;
+import jxl.write.*;
 
 /**
  * 
@@ -11,25 +13,54 @@ public class Schedule extends Observable {
      * 
      */
     public Schedule() {
-    	this.isCurrent = false;
+    	this.current = false;
     }
 
     /**
-     * 
+     * Denotes whether the schedule (the Tasks linked by this.tasks are current and should be used)
      */
-    private boolean isCurrent;
+    private boolean current;
 
     /**
-     * @param tasks 
-     * @return
+     * References to all the tasks used to make the schedule 
+     */
+    private Set<Task> tasks;
+
+    /**
+     * @param tasks the collection of Task references, in no particular order
+     * @return a reference to this schedule. The tasks will have been assigned starTimes
      */
     public Schedule generateSchedule(Set<Task> tasks) {
         // TODO implement here
+    	this.tasks = tasks;
+    	this.current = true;
         return null;
     }
     
-    public boolean getIsCurrent() {
-    	return isCurrent;
+
+    /**
+     * @return whether the Schedule is current.
+     */
+    public boolean isCurrent() {
+    	return this.current;
     }
 
+    /**
+     * Sets the Schedule to not current
+     * @return whether the Schedule is current (it won't be)
+     */
+    public boolean invalidate() {
+    	current = false;
+    	return current;
+    }
+
+    public WritableWorkbook toXLS() {
+    	if (this.current) {
+    		/** export the schedule **/
+    	} else {
+    		/** don't **/
+    	}
+
+    	return null;    	
+    }
 }
