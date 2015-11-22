a826f50c236b36e3970490c59227aea84d647d1f
Peter Chou <peterchou@peters-mbp.attlocal.net>
2015-11-21 21:43:24 -0800

    updated add/edit task controllers

--- src/Controllers/Tasks/EditTaskController.java
+++ src/Controllers/Tasks/EditTaskController.java
@@ -3,6 +3,7 @@ package Controllers.Tasks;
 import java.util.ArrayList;
 
 import Controllers.Controller;
+import Entities.Resource;
 import Entities.Task;
 
 /**
@@ -16,8 +17,13 @@ public class EditTaskController implements Controller {
     public EditTaskController() {
     }
 	
-	public void executeEditTask(String taskName, String taskID, Integer taskDuration, ArrayList<Task> predecessorTask, Task parentTask) {
-		project.editTask(taskName, taskID, taskDuration, predecessorTask, parentTask);
+	public void executeEditTask(String taskName, 
+			String taskID, 
+			Integer taskDuration, 
+			ArrayList<Task> predecessorTask, 
+			Task parentTask, 
+			ArrayList<Resource> taskResources) {
+		project.editTask(taskName, taskID, taskDuration, predecessorTask, parentTask, taskResources);
 	}
 
 }
