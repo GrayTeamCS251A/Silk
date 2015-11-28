515bf16e88395e523ef4c16d5948ad33e275ecf9
Peter Chou <peterchou@peters-mbp.attlocal.net>
2015-11-28 11:33:14 -0800

    updated project start time display

--- src/Controllers/Project/EditProjectController.java
+++ src/Controllers/Project/EditProjectController.java
@@ -2,6 +2,8 @@ package Controllers.Project;
 
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 import Controllers.*;
 
@@ -19,7 +21,7 @@ public class EditProjectController implements Controller {
 	public void executeEditProject(String projectName, Integer year, Integer month, Integer day, String projectAuthor) {
 		//Know that user did not input correct time
 		//Get original start time
-		if (year == 0)
+		if (String.valueOf(year).length() != 4)
 		{
 			year = project.getStartTime().get(Calendar.YEAR);
 			month = project.getStartTime().get(Calendar.MONTH);
@@ -29,6 +31,20 @@ public class EditProjectController implements Controller {
 		}
 		else
 		{
+			if (month >= 12)
+			{
+				Calendar localCalendar = Calendar.getInstance(TimeZone.getDefault());
+				month = localCalendar.get(Calendar.MONTH);
+			}
+			
+			Calendar localCalendar = new GregorianCalendar(year, month, day);
+						
+			if (day > localCalendar.getMaximum(Calendar.DAY_OF_MONTH))
+			{
+				System.out.println("Days exceeded!!!");
+				day = localCalendar.getMaximum(Calendar.DAY_OF_MONTH);
+			}
+					
 			project.updateInfo(projectName, year, month, day, projectAuthor);
 		}
 	}
